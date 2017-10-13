96f7cb779a256fc891097947678ab9db1149084d
Sultan Qasim Khan <sultanqasim@gmail.com>
2017-10-13 00:19:58 -0400

    qcom318-32: audio: make mixer_paths device specific
    We might need to make more things device specific later, but this at
    least gets audio mostly working on perry.
    
    Change-Id: Ief9a25cc7c08afbb2430b8226976b284f819fb58

--- qcom318-32.mk
+++ qcom318-32.mk
@@ -76,8 +76,7 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
